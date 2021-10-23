c0b7eb0420a0ad576ff19d2b2c1cf95c49871906
Никита <37046811+ahibis@users.noreply.github.com>
2021-10-24 02:22:10 +0300

    add block2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 const vue=new Vue({
     el:"#app",
     data:{
-        pic:1
+        pic:1,
+        d:0,
+        h:0,
+        s:0
     },
     computed:{
         picName(){
@@ -13,3 +16,10 @@ setInterval(e=>{
     vue.pic+=1;
     if(vue.pic==14) vue.pic=1;
 },5000)
+const time=new Date("10-31-2021").getTime();
+setInterval(e=>{
+    let date=new Date(time - new Date().getTime());
+    vue.d=date.getUTCDate();
+    vue.h=date.getUTCHours()
+    vue.s=date.getUTCMinutes();
+},1000)
