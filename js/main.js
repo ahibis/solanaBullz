280731e6d70f007b1b566329e0050c58dd28aceb
Никита <37046811+ahibis@users.noreply.github.com>
2021-10-24 04:39:03 +0300

    block3

--- js/main.js
+++ js/main.js
@@ -4,11 +4,34 @@ const vue=new Vue({
         pic:1,
         d:0,
         h:0,
-        s:0
+        s:0,
+        y:0,
+        cards:13,
+        pics:[1,2,3]
     },
     computed:{
         picName(){
             return `./img/${this.pic}.png`
+        },
+        scroll(){
+            return this.y;
+        }
+    },
+    watch:{
+
+    },
+    methods:{
+        next(){
+            for(let i=0;i<3;i+=1){
+                this.pics[i]+=1;
+                if(this.pics[i]===this.cards) this.pics[i]=1;
+            }
+        },
+        prev(){
+            for(let i=0;i<3;i+=1){
+                this.pics[i]-=1;
+                if(this.pics[i]===-1) this.pics[i]=this.cards;
+            }
         }
     }
 })
@@ -23,3 +46,16 @@ setInterval(e=>{
     vue.h=date.getUTCHours()
     vue.s=date.getUTCMinutes();
 },1000)
+
+setInterval(e=>{
+    let y1=document.querySelector(".text1").offsetTop;
+    let y2=document.querySelector(".benefits").offsetTop;
+    let y = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    let width = window.innerWidth;
+    let k=0;
+    if(y>y1 && y<y2) k=(y-y1)/(y2-y1);
+    if(y>y2) k=1;
+    let x=156-2076*k;
+    //console.log(`${x} ${y} ${y1} ${y2}`)
+    vue.y= x*width/1440;
+},10)
